4ab5817c489a85c4b2057db0d49e7240953feb9e
Carson Curry <carsoncurry52@gmail.com>
2019-01-29 13:18:16 -0600

    website compiled

---
+++ assets/javascript/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        centerMode: true,
+        centerPadding: '325px',
+        infinite: true,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        responsive: [
+            {
+                breakpoint: 1530,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+})
+
+$('.left').click(function() {
+    $('.slider').slick('slickPrev');
+})
+
+$('.right').click(function() {
+    $('.slider').slick('slickNext');
+})
+
+document.onkeydown = function(event) {
+    switch (event.keyCode) {
+        case 37:
+            $('.slider').slick('slickPrev');
+            break;
+        case 39:
+            $('.slider').slick('slickNext');
+            break;
+    }
+}
